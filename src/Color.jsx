9e84ebebe69d2f37561d124293826b3e0ba6e0ef
Alberto Lanzafame <lanzafamea2001@gmail.com>
2025-01-27 03:28:00 +0100

    up

--- src/Color.jsx
+++ src/Color.jsx
@@ -1,34 +1,8 @@
 import React from "react";
+import Color from "./C"
 
 
 function Color ({ color }) => {
   return <li>{color.name}</li>;
 };
 
-
-function Colors = ({ colors }) => {
-  return (
-    <ul>
-      {colors.map((color) => (
-        <Color key={color.id} color={color} />
-      ))}
-    </ul>
-  );
-};
-
-const App = () => {
-  const colorArray = [
-    { id: 1, name: "Red" },
-    { id: 2, name: "Blue" },
-    { id: 3, name: "Green" },
-  ];
-
-  return (
-    <div>
-      <h1>Color List</h1>
-      <Colors colors={colorArray} />
-    </div>
-  );
-};
-
-export default App;
